23e8c2de3bbf4c5ddb7d7674cbb5fcab95faae5e
JvanBart4p <jvbart@4proces.nl>
2023-04-06 13:28:28 +0200

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+export default function Button() {
+  return (
+    <div>
+      <button className="button-home">Click here</button>
+    </div>
+  );
+}
+
+// const Button = () => {
+//   <button className="button-home">Click here</button>;
+// };
+
+// export default Button;
