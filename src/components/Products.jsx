7f6dceb71c19857d46fd4c286aceccfc2b728198
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2023-04-07 13:11:16 +0200

    header

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import SearchBar from "./tools/SearchBar";
 import { useState } from "react";
+import Link from "next/link";
+
 const Products = ({ data }) => {
   const [results, setResults] = useState(data);
   const [searchQuery, setSearchQuery] = useState("");
@@ -14,15 +16,17 @@ const Products = ({ data }) => {
       <div className="products">
         {results.map((product, index) => {
           return (
-            <div className="products__card" key={`${product.id}${index}`}>
-              <h3>{product.title}</h3>
-              <Image
-                src={product.images[0]}
-                width={100}
-                height={100}
-                alt={product.title}
-              />
-            </div>
+            <Link href={product.title} key={`${product.id}${index}`}>
+              <div className="products__card">
+                <h3>{product.title}</h3>
+                <Image
+                  src={product.images[0]}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                />
+              </div>
+            </Link>
           );
         })}
       </div>
