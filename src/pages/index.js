e6788e7fa68f4ebfe64c6b97c628976e9b1069cf
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2023-04-07 10:50:26 +0200

    mockdata

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Button from "../components/tools/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ dataObject }) {
   const [message, setMessage] = useState("");
   const [secondMessage, setSecondMessage] = useState("");
   const handleClick = () => {
@@ -14,19 +14,37 @@ export default function Home() {
       setSecondMessage("thanks for clicking");
     }
   }, [message]);
+  console.log(dataObject.products);
   return (
     <>
       <main className="home">
+        <h1>NextJS app Template</h1>
         <div className="home__section-one">
-          <h1>NextJS app Template</h1>
           <Button click={handleClick} />
           <h2>{message}</h2>
           <h2>{secondMessage}</h2>
           <Link href={"/dashboard"} passHref>
             Dashboard
           </Link>
+          {dataObject &&
+            dataObject.products &&
+            dataObject.products.map((product, index) => {
+              return <div key={`${product.id}${index}`}>{product.title}</div>;
+            })}
         </div>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  let dataObject = {};
+  const data = await fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((json) => (dataObject = json));
+  return {
+    props: {
+      dataObject,
+    },
+  };
 }
