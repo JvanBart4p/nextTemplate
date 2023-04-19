b1865a9ac837d2b370af00e3e02fe2032ecb88a0
JvanBart4p <jvbart@4proces.nl>
2023-04-19 12:03:56 +0200

    initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "dcgs-portal-3006",
+      name: "next-template-3009",
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "./node_modules/next/dist/bin/next.js",
